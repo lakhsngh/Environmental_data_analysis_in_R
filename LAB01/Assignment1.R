3ff82f5a9df86434726d893df6dd590eda61d5d1
Lakhvinder Singh <lsingh7@kent.edu>
2026-02-18 16:55:36 -0500

    Finished Assignment 1

--- LAB01/Assignment1.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(sf)
-
-p.counties <-"./CBW/County_Boundaries.shp"
-p.stations <-"./CBW/Non-Tidal_Water_Quality_Monitoring_Stations_in_the_Chesapeake_Bay.shp"
-
-d.stations <- sf::read_sf(p.stations)
-d.counties <- sf::read_sf(p.counties)
